24450bf4abe9463bce4b195ef38bdec0747d27d0
Michael Dimmitt <michaelgdimmitt@gmail.com>
2018-10-11 23:13:41 -0400

    pop logic

--- lib/stack.ex
+++ lib/stack.ex
@@ -7,10 +7,25 @@ defmodule Stack do
     send(pid, {:push, val})
   end
 
+  def pop(pid) do
+    send(pid, {:pop, self()})
+    val = send(pid, {:pop, self()})
+
+    receive do
+      {:pop, val} -> val
+    end
+
+  end
+
   def loop(state) do
     new_state =
-      recieve do
+      receive do
         {:push, val} -> [val | state]
+
+        {:pop, caller} ->
+          [head | new_state] = state
+        send(caller, {:pop, head})
+        new_state
       end
     loop(new_state)
   end
